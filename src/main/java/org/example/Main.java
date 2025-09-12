e8faa1d205d1dce52ef0a548dd14f2429d89370e
HalukAyt <halukaytis@hotmail.com>
2025-09-12 18:02:46 +0300

    tamamlandı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //point
+
+        Point p = new Point(0,0);
+        Point p2 = new Point(6,8);
+        System.out.println(p.distance(3,4));
+        System.out.println(p2.distance(p));
+
+        //player
+
+        Player player1 = new Player("Haluk", 200, Weapon.SWORD);
+        player1.losehealth(100);
+        System.out.println(player1.healthRemaining());
     }
 }
---
+++ src/main/java/org/example/Weapon.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public enum Weapon {
+    SWORD(50,1.2),
+    AXE(70,0.9),
+    BOW(40, 1.8),
+    DAGGER(30,2.5);
+
+
+    private int damage;
+    private double attackSpeed;
+
+    Weapon(int damage, double attackSpeed) {
+        this.damage = damage;
+        this.attackSpeed = attackSpeed;
+    }
+    public int getDamage() {
+        return damage;
+    }
+    public double getAttackSpeed() {
+        return attackSpeed;
+    }
+}
